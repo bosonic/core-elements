61eab3415d909e0e68c5e6af7ceb88d2de8371bd
Raphaël Rougeron <goldoraf@gmail.com>
2015-12-09 14:59:40 +0100

    b-slider now uses gestures mixin for pointer tracking

--- dist/bosonic-runtime.js
+++ dist/bosonic-runtime.js
@@ -1801,6 +1801,9 @@ var TAP_TRESHOLD = 10;
 
 Bosonic.Events = {
     created: function() {
+        if (this.shadowRoot) {
+            PointerEventsPolyfill.dispatcher.register(this.shadowRoot);
+        }
         for (var eventName in this.listeners) {
             this.listen(this, eventName, this.listeners[eventName]);
         }
@@ -1911,47 +1914,6 @@ Bosonic.Events = {
 
     _getListenerKey: function(eventName, methodName) {
         return eventName + ':' + methodName;
-    },
-
-    trackPointer: function(initialEvent, callbackName, stopCallbackName) {
-        if (!this[callbackName]) {
-            throw 'Event handler method `' + callbackName + '` is not defined';
-        }
-        if (stopCallbackName !== undefined && !this[stopCallbackName]) {
-            throw 'Event handler method `' + stopCallbackName + '` is not defined';
-        }
-        var state = {
-            startX: initialEvent.clientX,
-            startY: initialEvent.clientY
-        };
-        this.__boundTrackingHandler = this._onTrackingPointerMove.bind(this, state, callbackName);
-        this.__boundStopTrackingHandler = this._onTrackingPointerUp.bind(this, state, stopCallbackName);
-        document.addEventListener('pointermove', this.__boundTrackingHandler);
-        document.addEventListener('pointerup', this.__boundStopTrackingHandler);
-    },
-
-    _onTrackingPointerMove: function(state, callbackName, event) {
-        var x = event.clientX,
-            y = event.clientY;
-        state.dx = x - state.startX;
-        state.dy = y - state.startY;
-        state.ddx = state.lastX ? x - state.lastX : 0;
-        state.ddy = state.lastY ? y - state.lastY : 0;
-        state.lastX = x;
-        state.lastY = y;
-
-        this[callbackName].call(this, state);
-    },
-
-    _onTrackingPointerUp: function(state, callbackName, event) {
-        document.removeEventListener('pointermove', this.__boundTrackingHandler);
-        document.removeEventListener('pointerup', this.__boundStopTrackingHandler);
-        delete this.__boundTrackingHandler;
-        delete this.__boundStopTrackingHandler;
-
-        if (callbackName !== undefined) {
-            this[callbackName].call(this, state);
-        }
     },
 
     customEvents: {
